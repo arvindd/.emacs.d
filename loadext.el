d2ac331d17da4c61205b1496ea0fdc28ef65ceda
arvindd <1006084+arvindd@users.noreply.github.com>
2020-07-30 05:34:47 +0530

    Added crypt functions

--- loadext.el
+++ loadext.el
@@ -1,9 +1,22 @@
-(require 'dockerfile-mode)
-(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+;;;; This file is for auto-enabling all extensions we use
+;;
 
+;; Get our color-theme right
 (require 'color-theme)
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)
-          (color-theme-resolve)))
+        (color-theme-resolve)))
+
+;; Enable automatic encryption and decryption
+(require 'epa-file)
+(epa-file-enable)
+
+;; Specifically for org-mode, enable crypting individual entries
+(require 'org-crypt)
+
+;; Enable docker mode when a docker-file is opened.
+(require 'dockerfile-mode)
+(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
 
--- orgsettings.el
+++ orgsettings.el
@@ -6,6 +6,18 @@
 (add-hook 'org-mode-hook
   (lambda ()
 	    
-      ;; Let RET follow links (like  C-c-o)
-      (setq org-return-follows-link t)
+    ;; Let RET follow links (like  C-c-o)
+    (setq org-return-follows-link t)
+
+    ;; Enable automatic crypting of org files
+    ;; All entries with tag ":crypt:" will be encrypted
+    ;; If you want to change this tag name, uncomment the following
+    ;; line, and replace "crypt" with your own tag name
+    ;; (setq org-crypt-tag-matcher "crypt")
+    (org-crypt-use-before-save-magic)
+    (setq org-tags-exclude-from-inheritance (quote ("crypt")))
+
+    ;; GPG key to use for encryption
+    ;; Either the Key ID or set to nil to use symmetric encryption.
+    (setq org-crypt-key nil)
 ))
