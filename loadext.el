aafe72485999c5915851d9cda7ed878d237f317c
arvindd <1006084+arvindd@users.noreply.github.com>
2020-08-08 07:42:47 +0530

    Added magit package

--- loadext.el
+++ loadext.el
@@ -11,6 +11,9 @@
 ;; Enable encryption for org-mode
 (require 'org-crypt)
 
+;; Load magit for working with git
+(require 'magit)
+
 ;; Enable docker mode when a docker-file is opened.
 (require 'dockerfile-mode)
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
