aa6a88a774ca8c629308df6e9ce16d126daab719
arvindd <1006084+arvindd@users.noreply.github.com>
2020-08-28 05:36:11 +0530

    Enforced todo dependencies even with checkboxes

--- orgsettings.el
+++ orgsettings.el
@@ -64,6 +64,7 @@
     ;; Enforce depedencies in todos. Parent tasks cannot be made to DONE unless
     ;; all their children are made to DONE.
     (setq org-enforce-todo-dependencies t)
+    (setq org-enforce-todo-checkbox-dependencies t)    
     
     ;; GPG key to use for encryption
     ;; Either the Key ID or set to nil to use symmetric encryption.
