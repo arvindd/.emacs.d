14850f4feca4427c8ec0edc533b0b876bdbd2eaa
arvindd <1006084+arvindd@users.noreply.github.com>
2022-03-25 06:21:10 +0530

    Added keybinding to start picture mode.

--- keybindings.el
+++ keybindings.el
@@ -1,7 +1,8 @@
 ;; All non-org keybindings that we would like to have
 ;; All org-keybindings are in orgsettings.el
 
-;; Enable magit
 (define-key global-map (kbd "C-x g") 'magit-status)
+(define-key global-map (kbd "C-x p") 'picture-mode)
 (define-key global-map (kbd "C-c v") 'calendar)
 (define-key global-map (kbd "C-c d") 'diary)
+
