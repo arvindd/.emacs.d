b3584386d0e3f2bb7ea59e7b74f6734273bac9bc
arvindd <1006084+arvindd@users.noreply.github.com>
2023-02-01 19:27:58 +0530

    Fixed auto-install package bug.
    If auto-install-package-alist is not set, then, we simply
    jump off auto installing packages.

--- init.el
+++ init.el
@@ -50,8 +50,9 @@
 ;; add them to the package-selected-packages so that melpa
 ;; load below considers these packages
 (if (equal (boundp 'package-selected-packages) nil)
-    (setq package-selected-packages auto-install-package-list)
-    (append package-selected-packages auto-install-package-list))
+    (when (boundp 'auto-install-package-list)
+      (setq package-selected-packages auto-install-package-list)
+      (append package-selected-packages auto-install-package-list)))
 
 ;; Many packages come from MELPA, so we add the infra here
 (setq load-melpa-file (concat init-file-dir "/load-melpa.el"))
